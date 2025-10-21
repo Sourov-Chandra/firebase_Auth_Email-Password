8c7f79bd2d396324b1198d041a718b955f81212d
sourov-chandra <sourovtpi@yahoo.com>
2025-10-21 09:09:46 +0600

    added Logout authentications

---
+++ src/components/Logout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { getAuth, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+const Logout = () => {
+
+    const auth = getAuth(app);
+
+    const handleLogout = ()=>{
+        signOut(auth)
+          .then(() => {
+            // Sign-out successful.
+            alert("User signed out successfully.")
+          })
+          .catch((error) => {
+            // An error happened.
+            console.log(error.message);
+          });
+    }
+  return (
+    <div className='mt-6'>
+      <button onClick={handleLogout} className="px-4 py-2 text-white bg-sky-600 rounded-md ">Logout</button>
+    </div>
+  );
+}
+
+export default Logout
